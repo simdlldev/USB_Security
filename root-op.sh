fee2bd5e3cbdba18a5f7a71e7364d1ea8dc2f0e9
simdll <146474174+simdlldev@users.noreply.github.com>
2024-10-28 13:49:10 +0100

    Update root-op.sh

--- root-op.sh
+++ root-op.sh
@@ -3,63 +3,81 @@
 timeStart='0'
 cCheck='0'
 userHome=$(cat /etc/usb-security.conf)
+
+baseDev=$(cat "/home/$userHome/.cache/usb/file/sys-dev")
+currentDev=$(ls /sys/bus/usb/devices/ | tr '\n' '#')
+
+for dev in /sys/bus/usb/devices/*
+do
+	localDev=$(echo "$dev" | cut -d/ -f6-)
+	if [[ ! "$localDev" == *"usb"* ]] ; then
+		cDev="$localDev#"
+		if [[ ! $baseDev =~ $cDev ]] ; then
+			echo "0" > "$dev/authorized"
+			echo "$localDev" > "/home/$userHome/.cache/usb/file/newDev"
+			sleep 1
+		fi
+	fi
+done
+
 for host in /sys/bus/usb/devices/usb*
 do
-	echo 0 > "$host/authorized_default"
+	echo 0 > $host/authorized_default
 done
+
+sleep 5
 val15=true
 while [[ $val15 = true ]] ; do
 	if [[ -f "/home/$userHome/.cache/usb/file/3done" ]] ; then
-		rm "/home/$userHome/.cache/usb/file/3done"
-		rm "/home/$userHome/.cache/usb/file/set3"
+		rm /home/$userHome/.cache/usb/file/3done
+		rm /home/$userHome/.cache/usb/file/set3
 		sleep 1
 	fi
-	uSettings=$(cat "/home/$userHome/.cache/usb/file/usb.conf")
+	uSettings=$(cat /home/$userHome/.cache/usb/file/usb.conf)
 	if [[ $cCheck == '0' ]] ; then
-		uSettingsOld=$(cat "/home/$userHome/.cache/usb/file/usb.conf")
+		uSettingsOld=$(cat /home/$userHome/.cache/usb/file/usb.conf)
 		cCheck='1'
 	fi
-	if [[ $uSettingsOld != "$uSettings" ]] ; then
+	if [[ $uSettingsOld != $uSettings ]] ; then
 		cCheck='0'
 		timeStart='0'
 	fi
 	if [[ $uSettings == '0' ]] ; then
 		for host in /sys/bus/usb/devices/usb*
 		do
-			echo 1 > "$host/authorized_default"
+			echo 1 > $host/authorized_default
 		done
 	elif [[ $uSettings == '1' ]] ; then
 		for host in /sys/bus/usb/devices/usb*
 		do
-			echo 1 > "$host/authorized_default"
+			echo 1 > $host/authorized_default
 		done
 		if [[ $timeStart == '0' ]] ; then
-			systemctl start usb-do-sec.service
+			$(systemctl start usb-do-sec.service)
 			timeStart='1'
 		fi
 	elif [[ $uSettings == '2' || $uSettings == '3' || $uSettings == '4' ]] ; then
 		for host in /sys/bus/usb/devices/usb*
 		do
-			echo 0 > "$host/authorized_default"
+			echo 0 > $host/authorized_default
 		done
 	fi
 	if [[ -f "/home/$userHome/.cache/usb/file/do" ]] ; then
-		path=$(cat "/home/$userHome/.cache/usb/file/do")
-		rm "/home/$userHome/.cache/usb/file/do"
-		echo "1" > "$path/authorized"
+		path=$(cat /home/$userHome/.cache/usb/file/do)
+		rm /home/$userHome/.cache/usb/file/do
+		echo "1" > $path/authorized
 	fi
 	if [[ -f "/home/$userHome/.cache/usb/file/ndo" ]] ; then
-		path=$(cat "/home/$userHome/.cache/usb/file/ndo")
-		rm "/home/$userHome/.cache/usb/file/ndo"
-		echo "0" > "$path/authorized"
+		path=$(cat /home/$userHome/.cache/usb/file/ndo)
+		rm /home/$userHome/.cache/usb/file/ndo
+		echo "0" > $path/authorized
 	fi
 	if [[ -f "/home/$userHome/.cache/usb/file/rm" ]] ; then
-		rm "/home/$userHome/.cache/usb/file/newDev"
-		rm "/home/$userHome/.cache/usb/file/rm"
+		rm /home/$userHome/.cache/usb/file/newDev
+		rm /home/$userHome/.cache/usb/file/rm
 	fi
 	sleep 1
 done
-
 
 
 
