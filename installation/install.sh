3d7b8d348a5a8ad90c7b3259b392718cf8a70880
simdll <146474174+simdlldev@users.noreply.github.com>
2024-10-28 13:30:04 +0100

    Update install.sh

--- installation/install.sh
+++ installation/install.sh
@@ -3,22 +3,22 @@
 currentUser=$(whoami)
 currentDir=$(pwd)
 
-zenity --question --title="Installazione USB Security" --text="<span font='14'><b>$currentUser</b> è il tuo nome utente?</span>"
+zenity --question --title="USB Security Installation" --text="<span font='14'><b>$currentUser</b> is your username?</span>"
 
 if [[ $? == 1 ]] ; then
-	userName=$(zenity --entry --title="Installazione USB Security" --text="Inserisci il tuo nome utente:" --entry-text="$currentUser")
+	userName=$(zenity --entry --title="USB Security Installation" --text="Enter your username:" --entry-text="$currentUser")
 	if [[ $userName == "" ]] ; then
-		zenity --info --title="Installazione USB Security" --text="L'installazione è stata terminata dall'utente"
+		zenity --info --title="USB Security Installation" --text="Installation was terminated by user"
 		exit
 	fi
 else
 	userName=$currentUser
 fi
 
-zenity --question --title="Installazione USB Security" --text="<span font='14'><b>Vuoi avviare l'installazione?</b></span>"
+zenity --question --title="USB Security Installation" --text="<span font='14'><b>Do you want to start the installation?</b></span>"
 
 if [[ $? == 1 ]] ; then
-	zenity --info --title="Installazione USB Security" --text="L'installazione è stata terminata dall'utente"
+	zenity --info --title="USB Security Installation" --text="Installation was terminated by user"
 	exit
 else
 	mkdir "/home/$userName/.local/share/usb/"
@@ -36,7 +36,7 @@ else
 	echo -e "[Unit]\nDescription=USB Security run in system space\nAfter=local-fs.target\n\n[Service]\nWorkingDirectory=/home/$userName/.local/share/usb/scripts\nExecStart=/home/$userName/.local/share/usb/scripts/root-op.sh\nRestart=on-failure\n\n[Install]\nWantedBy=multi-user.target" > "$currentDir/.install/usb-security.service"
 	echo -e "[Unit]\nDescription=USB Security run in system space\nAfter=local-fs.target\n\n[Service]\nWorkingDirectory=/home/$userName/.local/share/usb/scripts\nExecStart=/home/$userName/.local/share/usb/scripts/root-sec.sh\nRestart=on-failure\n\n[Install]\nWantedBy=multi-user.target" > "$currentDir/.install/usb-do-sec.service"
 	echo -e "[Desktop Entry]\nType=Application\nExec=/home/$userName/.local/share/usb/scripts/user-run.sh\nHidden=false\nNoDisplay=false\nX-GNOME-Autostart-enabled=true\nName[it_IT]=USB Security\nName=USB Security\nComment[it_IT]=USB Security run in user space\nComment=USB Security run in user space" > "/home/$userName/.config/autostart/USB-Security.desktop"
-	echo -e "[Desktop Entry]\nType=Application\nName=USB Security\nGenericName=USB Security\nComment=A simple tool to protect the USB's ports on your PC\nExec=/home/$userName/.local/share/usb/scripts/manager.sh\nIcon=/home/$userName/.icons/usb-security.png\nTerminal=false\nCategories=Secutity;System;\nKeywords=usb;security;protect;sicurezza;protezione;" > "/home/$userName/.local/share/applications/usb-security.desktop"
+	echo -e "[Desktop Entry]\nType=Application\nName=USB Security\nGenericName=USB Security\nComment=A simple tool to protect the USB's ports on your PC\nExec=/home/$userName/.local/share/usb/scripts/manager.sh\nIcon=/home/$userName/.icons/usb-security.png\nTerminal=false\nCategories=Secutity;System;\nKeywords=usb;security;protect;sicurezza;protezione;" > "/home/$userName/.local/share/applications/usb-securiry.desktop"
 fi
 
 cp "$currentDir/.install/root-op.sh" "/home/$userName/.local/share/usb/scripts/root-op.sh"
@@ -46,19 +46,23 @@ cp "$currentDir/.install/run.sh" "/home/$userName/.local/share/usb/scripts/run.s
 cp "$currentDir/.install/usb.sh" "/home/$userName/.local/share/usb/scripts/usb.sh"
 cp "$currentDir/.install/manager.sh" "/home/$userName/.local/share/usb/scripts/manager.sh"
 cp "$currentDir/.install/usb-security.png" "/home/$userName/.icons/usb-security.png"
+
+zenity --info --title="USB Security Installation" --text="Remove all the USB attached devices, after that press OK"
+
+echo "$(ls /sys/bus/usb/devices/ | tr '\n' '#')" > "/home/$userName/.cache/usb/file/sys-dev"
+
 pkexec "$currentDir/.root-installer.sh" $currentDir $userName
 
 if [[ -f "$currentDir/fail" ]] ; then
 	if [[ $(cat "$currentDir/fail") =~ "1" ]] ; then
-		zenity --info --title="Installazione USB Security" --text="L'installazione è fallita: udev rule error"
+		zenity --info --title="USB Security Installation" --text="Installation failed: udev rule error"
 	elif [[ $(cat "$currentDir/fail") =~ "2" ]] ; then
-		zenity --info --title="Installazione USB Security" --text="L'installazione è fallita: systemd service error"
+		zenity --info --title="USB Security Installation" --text="Installation failed: systemd service error"
 	fi
 fi
 
 if [[ -f "$currentDir/done" ]] ; then
-	zenity --info --title="Installazione USB Security" --text="L'installazione è stata completata con successo, USB Security sarà attivo dal prossimo accesso"
+	zenity --info --title="USB Security Installation" --text="Installation has been completed successfully, USB Security will be active from the next login"
 else
-	zenity --info --title="Installazione USB Security" --text="Si è verificato un errore"
+	zenity --info --title="USB Security Installation" --text="An error occurred"
 fi
-
